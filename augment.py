0dd55ebc2014936b35c647e1053ea5d09349f79e
raphi-web <raphael.prinz1993@gmail.com>
2025-08-20 11:02:40 +0200

    update

--- augment.py
+++ augment.py
@@ -1,3 +1,4 @@
+
 import warnings
 from enum import Enum
 
@@ -26,8 +27,7 @@ class ToTensor:
         sample["mask"] = TF.to_tensor(np.concatenate((background, msk), axis=-1))
 
         for key in [k for k in sample.keys() if k != "mask"]:
-            sample[key] = np.clip(sample[key], 0,10000)
-            sample[key] = TF.to_tensor(sample[key].astype(np.float32) / 10_000)
+            sample[key] = TF.to_tensor(sample[key].astype(np.float32) / 255)
         return sample
 
 
@@ -36,8 +36,8 @@ def valid_augm(sample, size=512):
     return A.Compose(augms)(image=sample["image"], mask=sample["mask"])
 
 
-def test_augm(sample):
-    augms = [A.HorizontalFlip(p=0.1)]
+def test_augm(sample,size=512):
+    augms = [ A.Resize(height=size, width=size, p=1.0) ,A.HorizontalFlip(p=0.1)]
     return A.Compose(augms)(image=sample["image"], mask=sample["mask"])
 
 
@@ -52,22 +52,17 @@ def train_augm_heavy(sample, size=512):
         A.VerticalFlip(p=0.75),
         A.Downscale((0.5, 0.75), p=0.05),
         A.MaskDropout(max_objects=3, fill=0, fill_mask=0, p=0.1),
-        # color transforms
+        # # color transforms
         A.OneOf(
             [
                 A.RandomBrightnessContrast(
                     brightness_limit=0.3, contrast_limit=0.3, p=1
                 ),
                 A.RandomGamma(gamma_limit=(70, 130), p=1),
-                A.ChannelShuffle(p=0.2),
-                A.HueSaturationValue(
-                    hue_shift_limit=30, sat_shift_limit=40, val_shift_limit=30, p=1
-                ),
-                A.RGBShift(r_shift_limit=30, g_shift_limit=30, b_shift_limit=30, p=1),
             ],
             p=0.8,
         ),
-        # distortion
+        # # distortion
         A.OneOf(
             [
                 A.ElasticTransform(p=1),
@@ -81,7 +76,6 @@ def train_augm_heavy(sample, size=512):
         A.OneOf(
             [
                 A.GaussNoise(p=1),
-                A.MultiplicativeNoise(p=1),
                 A.Sharpen(p=1),
                 A.GaussianBlur(p=1),
             ],
@@ -126,7 +120,6 @@ def train_augm_medium(sample, size=512):
         A.OneOf(
             [
                 A.GaussNoise(p=1),
-                A.MultiplicativeNoise(p=1),
                 A.Sharpen(p=1),
                 A.GaussianBlur(p=1),
             ],
--- dataset.py
+++ dataset.py
@@ -1,5 +1,6 @@
 import numpy as np
 import rasterio
+import torch
 from torch.utils.data import Dataset as BaseDataset
 
 from augment import ToTensor, valid_augm, train_augm_light, train_augm_heavy, AugmentKind, test_augm, train_augm_medium
@@ -7,7 +8,10 @@ from augment import ToTensor, valid_augm, train_augm_light, train_augm_heavy, Au
 
 def load_multiband(path):
     src = rasterio.open(path, "r")
-    return (np.moveaxis(src.read(), 0, -1)).astype(np.uint32)
+    rast = src.read()
+    rast = np.clip(rast, 0,8_000) / 8_000
+    rast = (rast*255).astype(np.uint8)
+    return (np.moveaxis(rast, 0, -1))
 
 
 def load_grayscale(path):
@@ -16,21 +20,20 @@ def load_grayscale(path):
 
 
 class Dataset(BaseDataset):
-    def __init__(self, label_list, classes=None, size=128, train=False, augment_kind=AugmentKind.MEDIUM):
+    def __init__(self, label_list, classes=None, size=128, augment_kind=AugmentKind.MEDIUM):
         self.fns = label_list
         self.size = size
-        self.train = train
         self.to_tensor = ToTensor(classes=classes)
         self.load_multiband = load_multiband
         self.load_grayscale = load_grayscale
 
         match augment_kind:
             case AugmentKind.LIGHT:
-                self.augm = train_augm_light if train else valid_augm
+                self.augm = train_augm_light
             case AugmentKind.MEDIUM:
-                self.augm = train_augm_medium if train else valid_augm
+                self.augm = train_augm_medium
             case AugmentKind.HEAVY:
-                self.augm = train_augm_heavy if train else valid_augm
+                self.augm = train_augm_heavy
             case AugmentKind.VALID:
                 self.augm = valid_augm
             case AugmentKind.TEST:
@@ -41,13 +44,20 @@ class Dataset(BaseDataset):
         img = self.load_multiband(self.fns[idx].replace("labels", "images"))
         msk = self.load_grayscale(self.fns[idx])
 
-        if self.train:
-            data = self.augm({"image": img, "mask": msk}, self.size)
-        else:
-            data = self.augm({"image": img, "mask": msk}, self.size)
+        data = self.augm({"image": img, "mask": msk}, self.size)
         data = self.to_tensor(data)
 
         return {"x": data["image"], "y": data["mask"], "fn": self.fns[idx]}
 
     def __len__(self):
         return len(self.fns)
+
+
+if __name__ == "__main__":
+    import glob
+    label_list = glob.glob("data/labels/**/*.tif")
+    ds = Dataset(label_list,[1,2,3], 512, augment_kind=AugmentKind.TEST)
+    dl = torch.utils.data.DataLoader(ds, batch_size=1, shuffle=True)
+    for batch in dl:
+        print(batch["x"].shape, batch["x"].mean(), batch["x"].std())
+        #print(batch["y"].shape)
